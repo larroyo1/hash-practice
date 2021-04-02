b3c877222a7b78ca04a83486f15a37aa6314a82f
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-01 19:45:52 -0600

    added customer hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,12 +1,18 @@
 car = {
 "name" => "mustang",
 "color" => "black",
-"miles" => 12,600,
+"miles" => 12,600
 }
 
 animal = {
-  "name" = "dog",
-  "breed" = "boston terrier",
-  "color" = "black and white", 
+  "name" => "dog",
+  "breed" => "boston terrier",
+  "color" => "black and white"
 
+}
+
+customer = {
+  "name" => "John Smith",
+  "number_of_orders" => 50,
+  "email" => "johnsmith@email.com"
 }
